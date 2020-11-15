abb4825a210a1e04854a895189ea57fc131f59c6
Aman Shrestha <ashrest2@u.rochester.edu>
2020-11-15 15:28:41 -0500

    base templates added

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
 
-@app.route('/')
+
+app = Flask(__name__)
+application = app
+
+bootstrap = Bootstrap(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
+
+@app.route("/")
 def index():
-	return "<h1>Hello World!</h1>"
+    return "<h1>Test World!</h1>"
